2b2ca333d4c4b3fe0c48518a39a6268bd52f2e00
benjamin <bwilson1@ualberta.ca>
2021-10-12 20:03:12 -0600

    realized that the requirements require one file, moved NodeClass

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-import NodeClass
+class Node:
 
-x = NodeClass()
+    def __init__(self):
 
-x.move(2)
+        self.node = [3, 3, 1, 0, 0, 0]
 
-print(x)
+    def move(self, missionary, cannibal):
+
+        if self.node[2] == 1:  # if the boat is on the left side
+
+            if missionary > 0:
+                self.node[0] = self.node[0] - missionary
+
+                self.node[3] = self.node[3] + missionary
+
+            if cannibal > 0:
+                self.node[1] = self.node[1] - cannibal
+
+                self.node[4] = self.node[4] - cannibal
+
+            self.node[2] = 0
+            self.node[5] = 1
+
+        else:  # if the boat is on the right side
+
+            if missionary > 0:
+                self.node[3] = self.node[3] - missionary
+
+                self.node[0] = self.node[0] + missionary
+
+            if cannibal > 0:
+                self.node[4] = self.node[4] - cannibal
+
+                self.node[1] = self.node[1] - cannibal
+
+            self.node[5] = 0
+            self.node[2] = 1
+
